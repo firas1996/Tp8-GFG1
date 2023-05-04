34132752f68d30a9238756f26f84a10ff780e990
firas1996 <firas.hm.1996@gmail.com>
2023-05-04 11:29:14 +0200

    Ajoutez des fichiers projet.

---
+++ LibraryAPI/Migrations/20230504085016_last.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LibraryAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class last : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "CoverUrl", "Description", "Rating", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Author 1", "https://www.book1.com", "Description 1", 3.8999999999999999, "Title 1" },
+                    { 2, "Author 2", "https://www.book2.com", "Description 2", 3.8999999999999999, "Title 2" },
+                    { 3, "Author 3", "https://www.book3.com", "Description 3", 3.8999999999999999, "Title 3" },
+                    { 4, "Author 4", "https://www.book4.com", "Description 4", 3.8999999999999999, "Title 4" },
+                    { 5, "Author 5", "https://www.book5.com", "Description 5", 3.8999999999999999, "Title 5" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
---
+++ LibraryAPI/Models/LibraryDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.Models
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public virtual DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasData(
+                new Book { Id=1,Title="Title 1",Description="Description 1",Author="Author 1",Rating=3.9,CoverUrl="https://www.book1.com"},
+                new Book { Id=2,Title="Title 2",Description="Description 2",Author="Author 2",Rating=3.9,CoverUrl="https://www.book2.com"},
+                new Book { Id=3,Title="Title 3",Description="Description 3",Author="Author 3",Rating=3.9,CoverUrl="https://www.book3.com"},
+                new Book { Id=4,Title="Title 4",Description="Description 4",Author="Author 4",Rating=3.9,CoverUrl="https://www.book4.com"},
+                new Book { Id=5,Title="Title 5",Description="Description 5",Author="Author 5",Rating=3.9,CoverUrl="https://www.book5.com"}
+                );
+        }
+    }
+}
